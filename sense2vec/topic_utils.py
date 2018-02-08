cba7b2e3ad17b1da4c7da6ed4cb6658d36f4bbb0
Amogh M S <msamogh@gmail.com>
2018-02-08 14:53:40 +0530

    Changed topic_similarity_map to weighted vector sum

--- sense2vec/topic_utils.py
+++ sense2vec/topic_utils.py
@@ -16,26 +16,30 @@ Comparison = namedtuple('Comparison', ['score', 'matched_topic', 'matched_varian
 def prettify_topic(x):
     return x.split('|')[0].replace('_', ' ')
 
-
 def uglify_topic(x):
     return x.replace(' ', '_') + '|NOUN'
 
+def split_tokens(x):
+    return x.split('|')[0].split('_')
 
-def find_valid_case_combination(topic):
+def merge_tokens(x):
+    return '_'.join(x) + '|NOUN'
+
+
+def find_best_casing(topic):
     """If the originally entered case-variant is not available, it looks for te first valid case-variant. It is greedy towards lowercase variants. Returns None if none of them are valid."""
     topic = unicode(topic)
-    # If original case combination is valid, return it
     if topic in sense_vec_model:
         return topic
 
     freqs = []
     # Generates all permutations of cases
-    tokens = topic.split('|')[0].split('_')
+    tokens = split_tokens(topic)
     param_for_product = [[0, 1]] * len(tokens)
 
     for casing_combination in product(*param_for_product):
         casing = [tokens[i].title() if casing_combination[i] else tokens[i].lower() for i in range(len(tokens))]
-        repr = '_'.join(casing) + '|NOUN'
+        repr = merge_tokens(casing)
         if repr in sense_vec_model:
             freqs.append((sense_vec_model[repr][0], repr))
     return max(freqs)[1] if freqs else None
@@ -44,14 +48,13 @@ def find_valid_case_combination(topic):
 def generate_variants(topic):
     if '|' not in topic:
         return []
-    tokens = topic.split('|')[0].split('_')
+    tokens = split_tokens(topic)
     variants = []
     for i in range(len(tokens)):
         variants.append(tokens[i:])
         variants.append(tokens[:-i])
-    variants = [find_valid_case_combination(uglify_topic(x)) for x in variants]
-    variants = filter(lambda x : x and x != '|NOUN', variants)  # Remove empty strings
-    variants = map(lambda topic : topic.split('|')[0].split('_'), variants)
+    variants = [find_best_casing(uglify_topic(x)) for x in variants if x and x != '|NOUN']
+    variants = map(split_tokens, variants)
 
     # Remove proper subsets
     proper_subsets = []
@@ -60,31 +63,32 @@ def generate_variants(topic):
             if ' '.join(variants[j]).lower() in ' '.join(variants[i]).lower():
                 proper_subsets.append(variants[j])
     unique = set(map(tuple, variants)) - set(map(tuple, proper_subsets))
-    return sorted([unicode(uglify_topic(x)) for x in unique], key=lambda x : len(x.split('|')[0].split('_')), reverse=True)
-
-
-def get_top_items(topic, n=1000):
-    topic = find_valid_case_combination(unicode(topic))
-    try:
-        token = sense_vec_model[topic][1]
-        related_items = sense_vec_model.most_similar(token, n)[0]
-    except Exception as e:
-        return []
-
-    for i in range(len(related_items)):
-        related_items[i] = {'text': prettify_topic(related_items[i]), 'similarity': sense_vec_model_similarity(topic, related_items[i]).similarity}
-        if related_items[i]['similarity'] < 0.6:
-            break
-
-    return related_items[:i]
-
+    return sorted([unicode(uglify_topic(x)) for x in unique], key=lambda x : len(split_tokens(x)), reverse=True)
 
 
 def topic_similarity_map(topics1, topics2, user_defined_taxonomy):
+    def populate_with_variants(topics):
+        all_topics = []
+        for t in topics:
+            variants = generate_variants(t)
+            all_topics.extend([{'topic': variant, 'valid': True} for variant in variants])
+
+    topics_from_query = populate_with_variants(topics1)
+    topics_from_knowledge_item = populate_with_variants(topics2)
+
+    def weighted_vector_sum(topics):
+        result = sense_vec_model[topics[0]][1] / sense_vec_model[topics[0]][0]
+        for topic in topics[1:]:
+            result += sense_vec_model[topic][1] / sense_vec_model[topic][0]
+        return result
+
+    return cosine_similarity(
+        weighted_vector_sum(topics_from_query).reshape(1, -1), 
+        weighted_vector_sum(topics_from_knowledge_item).reshape(1, -1)
+    )
 
 
-
-def topic_similarity_map(topics1, topics2, user_defined_taxonomy):
+def topic_similarity_map_2(topics1, topics2, user_defined_taxonomy):
     """
     user_defined_taxonomy is referred to when either
     1. A topic in `topics1_variants` is not in sense2vec model.
@@ -167,30 +171,12 @@ def topic_similarity_map(topics1, topics2, user_defined_taxonomy):
 
 def sense_vec_model_similarity(a, b):
     """Returns the cosine similarity between two Sense2Vec phrases and their Sense2Vec popularity ranks."""
-    
     try:
         f1, v1 = sense_vec_model[unicode(a)]
         f2, v2 = sense_vec_model[unicode(b)]
         v1 = v1.reshape(1, -1)
         v2 = v2.reshape(1, -1)
         sim = cosine_similarity(v1, v2)[0][0]
-        # print a, 'vs', b, '=', sim
         return SimilarityAndRank(similarity=round(sim * 100) / 100, rank1=float(f1), rank2=float(f2))
     except Exception as e:
-        print('An exception occurred!!!')
-        print(e)
         return SimilarityAndRank(similarity=0, rank1=float('inf'), rank2=float('inf'))
-
-
-# def get_index_in_rankings(token, search_term, nearest_items, n=20000):
-#     token = sense_vec_model[unicode(token)][1]
-#     try:
-#         index = nearest_items.index(unicode(search_term))
-#         return index
-#     except ValueError as e:
-#         return float('inf')
-
-
-# def get_index_ranking_product(a, b, nearest_a):
-#     result = get_index_in_rankings(a, b, nearest_a) * get_index_in_rankings(b, a, nearest_b)
-#     return result
