4133ee461935ce2ed09a4700055d4b0914eab35a
Amogh M S <msamogh@gmail.com>
2018-02-10 22:15:20 +0530

    Code cleanup

--- sense2vec/clarify.py
+++ sense2vec/clarify.py
@@ -1,13 +1,10 @@
-from itertools import combinations
-
-from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.cluster import AffinityPropagation
 import numpy as np
 
 from sense import sense_vec_model
 
 
-def find_most_representative_topic(candidate_topics, generality_threshold=1000, window_size=10, patience=500):
+def find_most_representative_topic(candidate_topics, generality_threshold=1000, window_size=10, patience=300):
     """Returns topic most representative of a set of topics.
 
     Args:
@@ -24,44 +21,24 @@ def find_most_representative_topic(candidate_topics, generality_threshold=1000, 
             flag = i
             break
     else:
-        print('no flag')
         return max(map(lambda topic : (sense_vec_model[topic][0], topic), candidate_topics))[1]
     return max(map(lambda topic : (sense_vec_model[topic][0], topic), candidate_topics[:max(40, flag)]))[1]
 
 
-def topic_sum_unit_vector(topics):
-    """Returns unit vector in the direction of sum of all topic vectors.
+def cluster_result_candidates(candidates, summary_type='abstractive_summary'):
+    """Clusters a list of candidates (obtained from first-level filtering) into a set of top-level topics for secondary questioning.
 
     Args:
-        topics: List of topic names.
+        candidates: Knowledge items classified as similar from (n-1)th pass.
+        summary_type: 'abstractive_summary' can use a representative topic that is not part of the cluster. 
+                      'extractive_summary' picks a representative topic from the cluster.
 
     Returns:
-        numpy.ndarray: Resultant vector.
+        list: List of tuples (representative topic, list of topics in the cluster).
     """
-    result = sum(sense_vec_model[topic][1] for topic in topics)
-    return result / np.linalg.norm(result)
-
-
-def vector_difference(a, b):
-    """Returns a - b.
-
-    Args:
-        a: Vector 1.
-        b: Vector 2.
-
-    Returns:
-        Unit vector in the direction of (a - b).
-    """
-    diff = a - b
-    return diff / np.linalg.norm(diff)
-
-
-def cluster_result_candidates(candidates):
-    """Clusters a list of candidates (obtained from first-level filtering) into a set of top-level topics for secondary questioning."""
     flatten_list = lambda l: [item for sublist in l for item in sublist]
-    def get_embedding(token):
-        print(token)
-        return sense_vec_model[token][1]
+    get_embedding = lambda token: sense_vec_model[token][1]
+
     all_topics = flatten_list(candidates)
     all_topics = map(lambda x : unicode(x['topic']), all_topics)
     # Just filter out for now
@@ -85,11 +62,10 @@ def cluster_result_candidates(candidates):
     # 2. Abstractive
     extractive_summary = map(lambda i : (all_topics[af.cluster_centers_indices_[i]], clusters[i]), range(len(clusters)))
     abstractive_summary = map(lambda x : (find_most_representative_topic(x), x), clusters)
-
     return {
         'extractive_summary': extractive_summary,
         'abstractive_summary': abstractive_summary
-    }
+    }[summary_type]
 
 
 def find_omitted_topic(query_topics, knowledge_item_topics):
@@ -97,18 +73,3 @@ def find_omitted_topic(query_topics, knowledge_item_topics):
     knowledge_item_topics = map(unicode, knowledge_item_topics)
     diff = vector_difference(topic_sum_unit_vector(query_topics), topic_sum_unit_vector(knowledge_item_topics))
     return find_most_representative_topic(sense_vec_model.most_similar(diff, 1000)[0])
-
-
-def uglify_topic(x):
-    return x.replace(' ', '_') + '|NOUN'
-
-
-if __name__ == '__main__':
-    while True:
-        try:
-            a = raw_input('a >> ')
-            b = raw_input('b >> ')
-            print(find_omitted_topic(map(uglify_topic, a.split(', ')), map(uglify_topic, b.split(', '))))
-        except Exception as e:
-            print(e)
-            continue
