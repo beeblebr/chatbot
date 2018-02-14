2e403cd5131396c1bf69fdee8541c751b47cb5a7
Amogh M S <msamogh@gmail.com>
2018-02-14 18:41:55 +0530

    Handled division by zero

---
+++ sense2vec/cluster.py
@@ -0,0 +1,57 @@
+from collections import namedtuple
+
+import numpy as np
+
+from sklearn.cluster import AffinityPropagation
+
+from sense import sense_vec_model
+
+Cluster = namedtuple(
+    'Cluster',
+    ['silhouette_score', 'af_model', 'topic_combination']
+)
+
+
+def fit_affinity_propagation_model(candidates):
+    """Fit an Affinity Propagation model to the list of topics.
+
+    The topics' Sense2Vec embeddings are used for clustering.
+
+    Args:
+        candidates: list
+            List of topics to cluster.
+
+    Returns:
+        af: The AffinityPropagation model fitted on the list.
+    """
+    candidates = map(lambda x: unicode(x), candidates)
+    # Just filter out for now
+    candidates = filter(lambda x: x in sense_vec_model, candidates)
+    embeddings = map(lambda token: sense_vec_model[token][1], candidates)
+
+    af = AffinityPropagation(verbose=True).fit(embeddings)
+    return af
+
+
+def group_samples_by_label(samples, labels):
+    """Group samples by their cluster labels.
+
+    Args:
+        samples: list
+            List of topics.
+        labels: list
+            Label map.
+
+    Returns:
+        clusters: list of lists
+            Each sublist contains members of a cluster.
+    """
+    clusters = []
+    for i in range(len(np.unique(labels))):
+        cluster = [
+            samples[x]
+            for x in range(len(labels))
+            if labels[x] == i
+        ]
+        clusters.append(cluster)
+    return clusters
--- sense2vec/comparison.py
+++ sense2vec/comparison.py
@@ -10,10 +10,12 @@ QUERY = 1
 KNOWLEDGE_ITEM = 2
 
 
+stop_words = get_stop_words_list()
+
+
 def populate_with_variants(
     topics,
     user_defined_taxonomy,
-    stop_words,
     query_or_knowledge_item
 ):
     """Fetch all possible variants for topics that are part of Sense2Vec vocabulary and populates topics necessary for user-defined taxonomy matching.
@@ -28,7 +30,7 @@ def populate_with_variants(
     """
     all_topics = []
     for topic in topics:
-        variants = generate_variants(topic, stop_words)
+        variants = generate_variants(topic)
         all_topics.extend([{'topic': variant, 'in_vocab': True}
                            for variant in variants])
         if query_or_knowledge_item == KNOWLEDGE_ITEM and not variants:
@@ -80,8 +82,6 @@ def topic_similarity_map(
     Returns:
         dict: Dict containing knowledge item and similarity score.
     """
-    stop_words = get_stop_words_list()
-
     topics_from_knowledge_item = knowledge_item['text']
 
     topics_from_query = populate_with_variants(
@@ -165,7 +165,7 @@ def fetch_search_results(
     pprint(max_similarity)
     max_similarity = max_similarity[0]['cosine_similarity']
     for i in range(len(all_results)):
-        all_results[i]['normalized_cosine_similarity'] = all_results[i]['cosine_similarity'] / max_similarity
+        all_results[i]['normalized_cosine_similarity'] = all_results[i]['cosine_similarity'] / min(1, max_similarity)
     subjective_ranking = sorted(all_results, key=lambda x: x['normalized_cosine_similarity'], reverse=True)
     pprint(subjective_ranking[:5])
 
