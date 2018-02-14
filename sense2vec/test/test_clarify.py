6a7a25bf158a44665b6eea5b5e77fa911c4d22b4
Amogh M S <msamogh@gmail.com>
2018-02-14 10:35:53 +0530

    Added tests

---
+++ sense2vec/test/test_clarify.py
@@ -0,0 +1,14 @@
+import pytest
+
+from clarify import find_most_representative_topic
+
+
+def test_find_most_representative_topic():
+    candidate_topics = [
+        'machine_learning|NOUN', 'natural_language_processing|NOUN'
+    ]
+
+    representative_topic = find_most_representative_topic(candidate_topics)
+
+    assert len(representative_topic) == 1
+    
